04489569b61a614fee10376e951ea68f6e74a6a9
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-03-11 13:28:49 +0300

    lockbox update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+"""
+A lockbox module
+"""
+
 
 def canUnlockAll(boxes):
     """
     This function determines if all boxes can be opened.
-    :param boxes: a list of lists, where each sublist represents a box and contains keys to other boxes
+    :param boxes: a list of lists, where each sublist represents a box and
+    contains keys to other boxes
     :return: True if all boxes can be opened, else False
     """
-    # The set of unlocked boxes, starting with box 0 since it is initially unlocked
+    # The set of unlocked boxes, starting with box 0 since it
+    # is initially unlocked
     unlocked = {0}
     # A list of keys, starting with the keys from the first box
     keys = boxes[0].copy()
@@ -21,10 +27,11 @@ def canUnlockAll(boxes):
             # and the box hasn't been unlocked yet, unlock it
             if 0 <= key < len(boxes) and key not in unlocked:
                 unlocked.add(key)  # Add the box to the set of unlocked boxes
-                new_keys.extend(boxes[key])  # Add the keys from the newly unlocked box
+                new_keys.extend(boxes[key])  # Add the keys from the newly box
 
         # Update the list of keys with the newly found keys
         keys = new_keys
 
-    # Check if we've unlocked all boxes (compare the number of unlocked boxes with the total number of boxes)
+    # Check if we've unlocked all boxes (compare the number of unlocked boxes
+    # with the total number of boxes)
     return len(unlocked) == len(boxes)
