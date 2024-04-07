c2d0f639b869a5741dcf8ac70a697679678e53cd
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-04-07 10:29:10 +0300

    Validation checker

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,36 +1,38 @@
 #!/usr/bin/python3
+"""
+This module contains a function that validates UTF-8 encoding
+"""
+
+
 def validUTF8(data):
-    # Function to check if a byte is a start of a UTF-8 sequence
-    def is_start_of_utf8(byte):
-        return byte >> 7 == 0b0 or (byte >> 5) == 0b110 \
-            or (byte >> 4) == 0b1110 or (byte >> 3) == 0b11110
+    """
+    Determines if a given data set represents a valid UTF-8 encoding
 
-    # Function to check if a byte is a continuation byte
-    def is_continuation_byte(byte):
-        return (byte >> 6) == 0b10
+    Args:
+        data (list(int)): A list of integers representing 1 byte of data each
 
-    # Loop through each byte in the data
-    i = 0
-    while i < len(data):
-        byte = data[i]
-        if is_start_of_utf8(byte):
-            # Determine the number of bytes in the sequence
-            if byte >> 7 == 0b0:  # 1-byte character
-                length = 1
-            elif (byte >> 5) == 0b110:  # 2-byte character
-                length = 2
-            elif (byte >> 4) == 0b1110:  # 3-byte character
-                length = 3
-            elif (byte >> 3) == 0b11110:  # 4-byte character
-                length = 4
+    Returns:
+        bool: True if data is a valid UTF-8 encoding, else False
+    """
+    if data is None or len(data) == 0:
+        return False
+
+    num_bytes = 0
+    for byte in data:
+        if num_bytes == 0:
+            if byte >> 5 == 0b110:
+                num_bytes = 1
+            elif byte >> 4 == 0b1110:
+                num_bytes = 2
+            elif byte >> 3 == 0b11110:
+                num_bytes = 3
+            elif byte >> 7 == 0b1:
+                return False
             else:
-                return False  # Invalid start byte
-            # Check the following bytes in the sequence
-            for j in range(1, length):
-                if i + j >= len(data) or not is_continuation_byte(data[i + j]):
-                    return False
-            # Move to the next byte after the sequence
-            i += length
+                return False
         else:
-            return False  # Invalid start of UTF-8 sequence
-    return True
+            if byte >> 6 != 0b10:
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
