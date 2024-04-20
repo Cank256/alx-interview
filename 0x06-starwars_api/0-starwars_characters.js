e43566faa0704b8fc625f8af048afd1c1138d228
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-04-20 09:39:41 +0300

    Starwars API update 4

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,38 +1,34 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-/**
- * Fetches characters from a Star Wars movie and prints their names
- * @param {string} movieId - The ID of the Star Wars movie
- */
-function getStarWarsCharacters (movieId) {
-  const url = `https://swapi.dev/api/films/${movieId}/`;
-  request(url, (error, response, body) => {
-    if (error) {
-      console.error('Error:', error);
-    } else {
-      if (response.statusCode === 200) {
-        const film = JSON.parse(body);
-        film.characters.forEach(characterUrl => {
-          request(characterUrl, (error, response, body) => {
-            if (!error && response.statusCode === 200) {
-              const character = JSON.parse(body);
-              console.log(character.name);
-            }
-          });
-        });
-      } else {
-        console.error('Failed to fetch movie data');
-      }
-    }
-  });
-}
-
 const movieId = process.argv[2];
-if (!movieId || isNaN(movieId)) {
-  console.error('Usage: ./0-starwars_characters.js <movieId>');
-  process.exit(1);
-}
 
-getStarWarsCharacters(movieId);
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
+
+request(`${apiUrl}${movieId}/`, { json: true }, (err, res, data) => {
+  if (err) {
+    console.log('Error:', err);
+    return;
+  }
+
+  if (res.statusCode !== 200) {
+    console.log(`Status Code: ${res.statusCode}`);
+    return;
+  }
+
+  data.characters.forEach((url) => {
+    request(url, { json: true }, (err, res, character) => {
+      if (err) {
+        console.log('Error:', err);
+        return;
+      }
+
+      if (res.statusCode !== 200) {
+        console.log(`Status Code: ${res.statusCode}`);
+        return;
+      }
+
+      console.log(character.name);
+    });
+  });
+});
