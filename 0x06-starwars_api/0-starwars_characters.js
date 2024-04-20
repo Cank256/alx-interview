c81f8cea25de75386537f731696de7340f522789
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-04-20 09:53:53 +0300

    Starwars API update 4

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,34 +1,47 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 const request = require('request');
-const movieId = process.argv[2];
 
-const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
-
-request(`${apiUrl}${movieId}/`, { json: true }, (err, res, data) => {
-  if (err) {
-    console.log('Error:', err);
-    return;
-  }
-
-  if (res.statusCode !== 200) {
-    console.log(`Status Code: ${res.statusCode}`);
-    return;
-  }
-
-  data.characters.forEach((url) => {
-    request(url, { json: true }, (err, res, character) => {
-      if (err) {
-        console.log('Error:', err);
-        return;
-      }
-
-      if (res.statusCode !== 200) {
-        console.log(`Status Code: ${res.statusCode}`);
-        return;
-      }
-
-      console.log(character.name);
+/**
+ * Fetches characters from a Star Wars movie and prints their names
+ * @param {string} movieId - The ID of the Star Wars movie
+ */
+async function getStarWarsCharacters(movieId) {
+  const movieUrl = `https://swapi.dev/api/films/${movieId}/`;
+  
+  try {
+    const movieResponse = await new Promise((resolve, reject) => {
+      request(movieUrl, (error, response, body) => {
+        if (error) reject(error);
+        else if (response.statusCode === 200) resolve(JSON.parse(body));
+        else reject(new Error('Failed to fetch movie data'));
+      });
     });
-  });
-});
+
+    const characters = await Promise.all(movieResponse.characters.map(characterUrl => {
+      return new Promise((resolve, reject) => {
+        request(characterUrl, (error, response, body) => {
+          if (error) reject(error);
+          else if (response.statusCode === 200) {
+            const character = JSON.parse(body);
+            resolve(character.name);
+          } else {
+            reject(new Error('Failed to fetch character data'));
+          }
+        });
+      });
+    }));
+
+    console.log(characters.join('\n'));
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+const movieId = process.argv[2];
+if (!movieId || isNaN(movieId)) {
+  console.error('Usage: ./0-starwars_characters.js <movieId>');
+  process.exit(1);
+}
+
+getStarWarsCharacters(movieId);
